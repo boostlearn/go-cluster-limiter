f3bd43c5e42c97eed43fe2df286bbedcaa5f1550
刘广开 <bjliuguangkai@corp.netease.com>
2020-07-24 18:16:07 +0800

    add

--- cluster_counter/store.go
+++
@@ -1,8 +0,0 @@
-package cluster_counter
-
-import "time"
-
-type DataStoreI interface {
-	Store(name string, startTime time.Time, endTime time.Time, lbs map[string]string, value int64) error
-	Load(name string, startTime time.Time, endTime time.Time, lbs map[string]string) (int64, error)
-}
