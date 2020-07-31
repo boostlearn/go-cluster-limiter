119f920714c2440fe29a4c61aae7aa307ad312bb
刘广开 <bjliuguangkai@corp.netease.com>
2020-07-31 09:12:26 +0800

    add

--- tests/cluster_limiter_benchmark_test.go
+++ tests/cluster_limiter_benchmark_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"github.com/boostlearn/go-cluster-limiter/cluster_counter"
+	"github.com/boostlearn/go-cluster-limiter/cluster_counter/redis_store"
 	"github.com/boostlearn/go-cluster-limiter/cluster_limiter"
 	"log"
 	"math/rand"
@@ -15,16 +16,18 @@ var counter *cluster_counter.ClusterCounter
 var limiter *cluster_limiter.ClusterLimiter
 
 func init() {
-	factory := cluster_counter.NewFactory(
+	counterStore := redis_store.NewStore("127.0.0.1:6379", "", "blcl:")
+
+	counterFactory := cluster_counter.NewFactory(
 		&cluster_counter.ClusterCounterFactoryOpts{
 			Name:                     "",
-			DefaultLocalTrafficRatio: 0.1,
+			DefaultLocalTrafficRatio: 1.0,
 			HeartbeatInterval:        100 * time.Millisecond,
 		},
-		nil )
-	factory.Start()
+		counterStore)
+	counterFactory.Start()
 
-	counter, _ = factory.NewClusterCounter(
+	counter, _ = counterFactory.NewClusterCounter(
 		&cluster_counter.ClusterCounterOpts{
 			Name:                     "test",
 			BeginTime:                time.Time{},
@@ -41,7 +44,7 @@ func init() {
 			HeartbeatInterval: 100 * time.Millisecond,
 			InitLocalTrafficRatio:  1.0,
 		},
-		nil)
+		counterStore)
 	limiterFactory.Start()
 
 	var err error
@@ -62,16 +65,16 @@ func init() {
 	limiter.SetTarget(100000)
 }
 
-func initNumber() {
-	numbers = make([]int, 0, 50)
-	for i := 0; i < 50; i++ {
+func initNumber(cnt int) {
+	numbers = make([]int, 0, cnt)
+	for i := 0; i < cnt; i++ {
 		numbers = append(numbers, rand.Int())
 	}
 }
 
 func doSomething() {
 	sort.Ints(numbers)
-	rand.Shuffle(len(numbers), func(i, j int) { numbers[i], numbers[j] = numbers[j], numbers[i] })
+	//rand.Shuffle(len(numbers), func(i, j int) { numbers[i], numbers[j] = numbers[j], numbers[i] })
 }
 
 func doSomethingWithCounter() {
@@ -86,8 +89,8 @@ func doSomethingWithLimiter() {
 	doSomething()
 }
 
-func Benchmark_Single_Directly(b *testing.B) {
-	initNumber()
+func Benchmark_Single_Directly_10(b *testing.B) {
+	initNumber(10)
 
 	b.ReportAllocs()
 	b.ResetTimer()
@@ -96,8 +99,8 @@ func Benchmark_Single_Directly(b *testing.B) {
 	}
 }
 
-func Benchmark_Single_Counter(b *testing.B) {
-	initNumber()
+func Benchmark_Single_Counter_10(b *testing.B) {
+	initNumber(10)
 
 	b.ReportAllocs()
 	b.ResetTimer()
@@ -107,8 +110,70 @@ func Benchmark_Single_Counter(b *testing.B) {
 }
 
 
-func Benchmark_Single_Limiter(b *testing.B) {
-	initNumber()
+func Benchmark_Single_Limiter_10(b *testing.B) {
+	initNumber(10)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		doSomethingWithLimiter()
+	}
+}
+
+func Benchmark_Single_Directly_100(b *testing.B) {
+	initNumber(100)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		doSomething()
+	}
+}
+
+func Benchmark_Single_Counter_100(b *testing.B) {
+	initNumber(100)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		doSomethingWithCounter()
+	}
+}
+
+
+func Benchmark_Single_Limiter_100(b *testing.B) {
+	initNumber(100)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		doSomethingWithLimiter()
+	}
+}
+
+func Benchmark_Single_Directly_1000(b *testing.B) {
+	initNumber(1000)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		doSomething()
+	}
+}
+
+func Benchmark_Single_Counter_1000(b *testing.B) {
+	initNumber(1000)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		doSomethingWithCounter()
+	}
+}
+
+
+func Benchmark_Single_Limiter_1000(b *testing.B) {
+	initNumber(1000)
 
 	b.ReportAllocs()
 	b.ResetTimer()
